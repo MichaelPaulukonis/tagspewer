a145901289cd805ebf7ed6d5b347372f899c2978
Michael Paulukonis <xraysmalevich@gmail.com>
2015-12-04 17:04:12 -0500

    temp lexicon passed in to spew(), tests for same; failing tests added for whitespace (tabs); test re-org

--- test/tagspewer_test.js
+++ test/tagspewer_test.js
@@ -10,6 +10,11 @@ var tester = function() {
       Tagspewer = require('../lib/tagspewer'),
       spewer = new Tagspewer(),
       lexicon = require('../lib/lexicon.js'),
+      // the properties of a lexicon object are reffered to as "tags"
+      // they may or may not be valid pos-tags
+      // but they are what will be replaced, when surrounded by whitespace, in a template
+      // actually, that's something to test...
+      // what whitespace is recognized?
       smallLex = {
         "NN":  ["cat"],
         "VBZ": ["sits"],
@@ -19,31 +24,48 @@ var tester = function() {
 
   describe('tagspewer tests', function() {
 
-    it('Tagspewer should be a function', function() {
-      expect(Tagspewer).to.be.an('function');
+    describe('API', function() {
+
+      it('Tagspewer should be a function', function() {
+        expect(Tagspewer).to.be.an('function');
+      });
+
+      it('should return an object', function() {
+        expect(spewer).to.be.an('object');
+      });
+
+      it('should have a spew method', function() {
+        expect(spewer.spew).to.be.an('function');
+      });
+
+      it('should expose a (default) lexicon', function() {
+        expect(spewer.lexicon).to.be.an('object');
+      });
+
+      it('should take in a non-default lexicon, and expose it', function() {
+        var customLexicon = { "NNP": ["whales"] };
+        var customSpewer = new Tagspewer(customLexicon);
+        expect(customSpewer.lexicon).to.equal(customLexicon);
+      });
+
     });
 
-    it('should return an object', function() {
-      expect(spewer).to.be.an('object');
+
+    describe('lexicon tests', function() {
+
+      it('should auto-wrap a string-value as a single-element array', function() {
+        // if a lexicon property-value is a string
+        // it should be auto-encapsulated as an array on 1 element
+        var customLexicon = { "NNP": "whales" },
+            customSpewer = new Tagspewer(customLexicon);
+        expect(customSpewer.spew('NNP')).to.equal('whales');
+      });
     });
 
-    it('should have a spew method', function() {
-      expect(spewer.spew).to.be.an('function');
-    });
-
-    it('should expose a (default) lexicon', function() {
-      expect(spewer.lexicon).to.be.an('object');
-    });
-
-    it('should take in a non-default lexicon, and expose it', function() {
-      var customLexicon = { "NNP": ["whales"] };
-      var customSpewer = new Tagspewer(customLexicon);
-      expect(customSpewer.lexicon).to.equal(customLexicon);
-    });
 
     describe('spew tests', function() {
 
-      it('should return empty string with no parameters', function() {
+      it('should return empty string when called with no parameters', function() {
         var actual = spewer.spew();
         var expected = '';
         expect(actual).to.be.an('string');
@@ -64,6 +86,45 @@ var tester = function() {
         var expected = 'The cat sits on the futon.';
         var actual = smallSpewer.spew(templateWithNonTags);
         expect(actual).to.equal(expected);
+      });
+
+      it('should ignore known-tags NOT surrounded by whitespace', function() {
+        var smallSpewer = new Tagspewer(smallLex);
+        var templateNoTags = 'The -NN- sits on the futon.';
+        var expected = 'The -NN- sits on the futon.';
+        var actual = smallSpewer.spew(templateNoTags);
+        expect(actual).to.equal(expected);
+      });
+
+      it('should recognize tags surrounded by TABs', function() {
+        var smallSpewer = new Tagspewer(smallLex);
+        var templateTabs = 'The \tNN\t sits on the futon.';
+        var expected = 'The \tcat\t sits on the futon.';
+        var actual = smallSpewer.spew(templateTabs);
+        expect(actual).to.equal(expected);
+      });
+
+      it('should use a custom lexicon when provided', function() {
+        let spewer = new Tagspewer({ 'NN': ['cat']}),
+            lexicon = { 'NN': ['whale'] },
+            template = 'NN',
+            expected = 'whale',
+            actual = spewer.spew(template, lexicon);
+        expect(actual).to.equal(expected);
+      });
+
+      it('should use a custom lexicon when provided, and then use standard lexicon again', function() {
+        // ie, custom lexicon in spew() does not overwrite the object's lexicon
+        let lexiconStandard = { 'NN': ['cat']},
+            spewer = new Tagspewer(lexiconStandard),
+            lexiconCustom = { 'NN': ['whale'] },
+            template = 'NN',
+            expectedCustom = 'whale',
+            actualCustom = spewer.spew(template, lexiconCustom),
+            expectedStandard = 'cat',
+            actualStandard = spewer.spew(template);
+        expect(actualCustom).to.equal(expectedCustom);
+        expect(actualStandard).to.equal(expectedStandard);
       });
 
     });
